2574581d94a0d4630b406ee5e261517914c842ec
Jaimee Lamphiear <2010jlamphiear@gmail.com>
2020-11-19 00:04:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbow(colors)
-  puts "#{Colors}"
-  "Colors" = "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  puts "#{colors}""
+  "colors" = "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
   end
